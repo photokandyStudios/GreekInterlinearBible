fd6a51e9f510c24fa98fab810f55d2a922ca9ff1
Kerri Shotts <kerrishotts@gmail.com>
2012-01-20 19:07:30 -0600

    Initial Commit

---
+++ GreekInterlinearBible/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GreekInterlinearBible
+//
+//  Created by Kerri Shotts on 1/20/12.
+//  Copyright photoKandy Studios LLC 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
